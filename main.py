4cdc54dab7797517edff87cd3d2942c8095e6347
bloodofspring <t.shagin@gmail.com>
2024-10-27 11:13:11 +0300

    feat: Переделан код, логи вынесены в отдельный класс, переработаны методы в классе GetUserResponse

--- main.py
+++ main.py
@@ -13,6 +13,24 @@ from config import OWNER_ID
 from database.create import create_tables
 from database.models import Photos
 
+PROGRAM_STARTED: datetime = datetime.now()
+
+
+class LogCreator:
+    bot_started: str = Fore.LIGHTYELLOW_EX + "[!] Клиент пробужден! Ожидание ответа от пользователя..."
+    response_got: str= Fore.LIGHTYELLOW_EX + "[#] Ответ от пользователя получен! Сохранение..."
+    file_saved: str = Fore.LIGHTYELLOW_EX + "[#] Ответ от пользователя сохранен! Выключение..."
+    bot_stopped: str = Fore.LIGHTWHITE_EX + ("[!] Клиент отправлен в режим сна."
+                                        "\nВремя исполнения программы: {}"
+                                        "\nСледующее пробуждение: {}")
+
+    def info(self, log_name_or_text: str, *args):
+        if not hasattr(self, log_name_or_text):
+            print(log_name_or_text.format(*args))
+            return
+
+        print(getattr(self, log_name_or_text).format(*args))
+
 
 class BaseHandler:
     """Базовый обработчик-исполнитель"""
@@ -32,37 +50,40 @@ class BaseHandler:
         return self.HANDLER(self.func, self.FILTER)
 
 
-class GetPhoto(BaseHandler):
+class GetUserResponse(BaseHandler):
     FILTER = filters.photo
 
     async def func(self, _, request: Message):
         now = datetime.now()
+        username = request.from_user.username
 
-        if not os.path.exists("@mazutta_photos"):
-            os.mkdir("@mazutta_photos")
+        LogCreator().info(LogCreator.response_got)
 
-        file_name = (f"@mazutta_photos/Photo("
-                     f"date={now.day}_{now.month}_{now.year},"
+        if not os.path.exists(f"@{username}_photos"):
+            os.mkdir(f"@{username}_photos")
+
+        file_name = (f"@{username}/Photo("
+                     f"date={now.day}_{now.month}_{now.year}, "
                      f"time={now.hour}_{now.minute}_{now.second}"
                      f")")
         await request.download(file_name=file_name)
         Photos.create(file_name=file_name)
         await request.reply_text(text="Сохранение произведено успешно!")
 
+        LogCreator().info(LogCreator.file_saved)
+
         self.stop_client()
 
     @staticmethod
-    def stop_client():
-        exit(Fore.LIGHTWHITE_EX + f"[!] Клиент отправлен в режим сна. Следующее пробуждение: {create_new_time_point()}")
+    def set_new_awake_time() -> datetime:
+        now = datetime.now()
+        until_4_44 = 1724 - (now.minute + now.hour * 60)
+        send_time = now + timedelta(minutes=randint(until_4_44, until_4_44 + 1440))
 
+        return send_time
 
-def create_new_time_point():
-    # 1724 = 28 HRS 44 MINS
-    now = datetime.now()
-    until_4_44 = 1724 - (now.minute + now.hour * 60)
-    send_time = now + timedelta(minutes=randint(until_4_44, until_4_44 + 1440))
-
-    return send_time
+    def stop_client(self):
+        exit(LogCreator.bot_stopped.format(datetime.now() - PROGRAM_STARTED, self.set_new_awake_time()))
 
 
 def send_notification_to_mazutta() -> None:
@@ -70,17 +91,19 @@ def send_notification_to_mazutta() -> None:
 
 
 def add_handlers() -> None:
-    for handler in [GetPhoto]:
+    for handler in [GetUserResponse]:
         pyrogram_client.add_handler(handler().de_pyrogram_handler)
-    print("Все обработчики успешно добавлены!")
 
 
 def run_bot() -> None:
-    add_handlers()
-    create_tables()
     init(autoreset=True)
     send_notification_to_mazutta()
+
+    add_handlers()
+    create_tables()
+
     try:
+        LogCreator().info(LogCreator.bot_started)
         pyrogram_client.run()
     except Exception as e:
         print(f"Невозможно запустить клиента! {e}")
