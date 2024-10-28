940edcd5cdc3baafe0827dbf463890b71683dbc6
bloodofspring <t.shagin@gmail.com>
2024-10-28 16:25:24 +0300

    feat: fixed 'database is locked' peewee error

--- database/db_init.py
+++ database/db_init.py
@@ -2,4 +2,4 @@ from typing import Final
 
 from peewee import SqliteDatabase
 
-db: Final[SqliteDatabase] = SqliteDatabase("filenames_archive")
+db: Final[SqliteDatabase] = SqliteDatabase("filenames_archive", check_same_thread=False)
