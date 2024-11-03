408d2cc2847ef181d9ea4fd8d6e98605ca4aa261
bloodofspring <t.shagin@gmail.com>
2024-11-03 23:07:14 +0300

    feat: Добавлено удаление лишних файлов

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os.path
+
 from colorama import init
 from pyrogram import Client
 from pyrogram.types import Message
 
+from config import NAME
 from database.create import create_tables
 from file_downloader.FileDownloader import TxtDownloader, PicDownloader
 from filters import save_message_filter
@@ -16,9 +19,18 @@ async def save_message(_: Client, request: Message):
         await TxtDownloader(pyrogram_request=request).save_message()
 
 
+def remove_bot_journals() -> None:
+    if os.path.exists(f"{NAME}.session"):
+        os.remove(f"{NAME}.session")
+
+    if os.path.exists(f"{NAME}.session-journal"):
+        os.remove(f"{NAME}.session-journal")
+
+
 def run_bot() -> None:
     init(autoreset=True)
     create_tables()
+    remove_bot_journals()
     pyrogram_client.run()
 
 
