b522e6b2b53eb9455013c814fb06dec20eb490ff
Tim Ross <149096639+bloodofspring@users.noreply.github.com>
2024-11-26 17:20:07 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,28 @@
 #! /usr/bin/bash
+touch start_log.txt
 git pull
-python3 -m venv .venv
-.venv/bin/pip install -r requirements.txt
-.venv/bin/python main.py
+
+if python -V 2> start_log.txt
+then
+  echo "Python detected"
+  echo "creating virtual environment..."
+  python -m venv .venv || echo "Error while creating .venv"
+else
+  echo "No python detected. Searching for python3..."
+  if python3 -V 2> start_log.txt
+  then
+    echo "Python3 detected"
+    echo "Creating virtual environment..."
+    python3 -m venv .venv || echo "Error while creating .venv"
+  else echo "No python3 detected!"
+  fi
+fi
+
+if [ -d .venv ]
+then
+  echo "Installing requirements.txt..."
+  .venv/bin/pip install -r requirements.txt
+  echo "Starting code..."
+  .venv/bin/python main.py
+else "No virtual environment!"
+fi
